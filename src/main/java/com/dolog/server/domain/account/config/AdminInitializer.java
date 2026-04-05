ae11e6fc85993ab5f1bf3c0dc7ce56b66529f3d1
Yeonhee-Ha <78415838+Yeonhee-Ha@users.noreply.github.com>
2026-04-05 17:05:18 +0900

    ✨ Feat: Developer 계정 자동 생성 로직

---
+++ src/main/java/com/dolog/server/domain/account/config/AdminInitializer.java
@@ -0,0 +1,43 @@
+package com.dolog.server.domain.account.config;
+
+import com.dolog.server.domain.account.entity.Account;
+import com.dolog.server.domain.account.entity.enums.AccountStatus;
+import com.dolog.server.domain.account.entity.enums.Role;
+import com.dolog.server.domain.account.repository.AccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class AdminInitializer {
+
+    private final AccountRepository accountRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Value("${admin.email}")
+    private String adminEmail;
+
+    @Value("${admin.password}")
+    private String adminPassword;
+
+    @Bean
+    public CommandLineRunner initAdmin() {
+        return args -> {
+            if (!accountRepository.existsByEmail(adminEmail)) {
+
+                Account superAdmin = Account.builder()
+                        .email(adminEmail)
+                        .password(passwordEncoder.encode(adminPassword))
+                        .role(Role.DEVELOPER) // 슈퍼어드민
+                        .accountStatus(AccountStatus.ACTIVE)
+                        .build();
+
+                accountRepository.save(superAdmin);
+            }
+        };
+    }
+}
