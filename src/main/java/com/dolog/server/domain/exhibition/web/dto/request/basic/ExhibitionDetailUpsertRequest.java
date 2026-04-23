b4f14af691a2b312bf4d7aae30a7210ee0bb54e2
wonpark1 <2023111927@dgu.ac.kr>
2026-04-24 03:48:47 +0900

    🐛 Fix: setter 추가

--- src/main/java/com/dolog/server/domain/exhibition/web/dto/request/basic/ExhibitionDetailUpsertRequest.java
+++ src/main/java/com/dolog/server/domain/exhibition/web/dto/request/basic/ExhibitionDetailUpsertRequest.java
@@ -4,11 +4,13 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class ExhibitionDetailUpsertRequest {
 
