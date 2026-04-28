dc3ac42291c6b4af6f7b704f7a253f586ca5388c
jiwoo <ryann1203@naver.com>
2026-04-28 16:06:26 +0900

    🔨 Refactor: 작품 전체 정보 수정 Setter 추가

--- src/main/java/com/dolog/server/domain/artwork/web/dto/request/ArtworkUpdateFullRequest.java
+++ src/main/java/com/dolog/server/domain/artwork/web/dto/request/ArtworkUpdateFullRequest.java
@@ -2,12 +2,15 @@ package com.dolog.server.domain.artwork.web.dto.request;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class ArtworkUpdateFullRequest {
     private String title;
@@ -15,10 +18,11 @@ public class ArtworkUpdateFullRequest {
     private String category;
     private UUID zoneId;
     private String purchaseUrl;
-    private List<UUID> artistIds;
-    private List<ImageUpdateDto> images;
+    private List<UUID> artistIds = new ArrayList<>();
+    private List<ImageUpdateDto> images = new ArrayList<>();
 
     @Getter
+    @Setter
     @NoArgsConstructor
     public static class ImageUpdateDto {
         private UUID id;             // 선택 (기존 이미지 수정 시 필요, 신규 추가 시 null)
