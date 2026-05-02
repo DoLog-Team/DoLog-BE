fa223ce478fe99ddf5a4c83e13a13b47af52c920
ohchanju <ohchanju3@naver.com>
2026-05-03 00:51:17 +0900

    🔧 fix: SwaggerConfig 프로파일 제한 제거로 Authorize 버튼 복구

--- src/main/java/com/dolog/server/global/config/SwaggerConfig.java
+++ src/main/java/com/dolog/server/global/config/SwaggerConfig.java
@@ -6,11 +6,9 @@ import io.swagger.v3.oas.models.tags.Tag;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import java.util.List;
 
-@Profile("!prod")
 @Configuration
 public class SwaggerConfig {
 
