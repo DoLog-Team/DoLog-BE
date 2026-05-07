50a2d9376dcbe482ffbbce8a3b1c3c5103988279
jiwoo <ryann1203@naver.com>
2026-05-07 22:34:58 +0900

    🔨 Refac: 작품 등록 필드 null 허용

--- src/main/java/com/dolog/server/domain/artwork/web/dto/request/ArtworkCreateRequest.java
+++ src/main/java/com/dolog/server/domain/artwork/web/dto/request/ArtworkCreateRequest.java
@@ -15,34 +15,17 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ArtworkCreateRequest {
-    @NotNull(message = "작가 프로필 ID는 필수입니다.")
     private UUID artistProfileId;
-
-    @NotBlank(message = "작품 제목은 필수입니다.")
     private String title;
-
-    @NotBlank(message = "작품 카테고리는 필수입니다.")
     private String category;
-
     private String material;
-
     private String size;
-
-    @NotBlank(message = "작품 상세 설명은 필수입니다.")
     private String description;
-
     private UUID zoneId;
-
     private String purchaseUrl;
-
     private String youtubeUrl;
-
     private Integer orderIndex;
-
     private String artistRole;
-
-    @NotNull(message = "대표 이미지는 필수입니다.")
     private MultipartFile mainImageFile;
-
     private MultipartFile locationMapFile;
 }
