445d6d0501670563f2437ed04041c32442a09d14
wonpark1 <2023111927@dgu.ac.kr>
2026-05-15 13:34:10 +0900

    🐛 Fix: exhibition type 소문자 허용

--- src/main/java/com/dolog/server/domain/exhibition/entity/enums/ExhibitionType.java
+++ src/main/java/com/dolog/server/domain/exhibition/entity/enums/ExhibitionType.java
@@ -1,6 +1,19 @@
 package com.dolog.server.domain.exhibition.entity.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum ExhibitionType {
     GRADUATION,   // 졸업 전시
-    ASSIGNMENT    // 과제전
+    ASSIGNMENT;   // 과제전
+
+    @JsonCreator
+    public static ExhibitionType from(String value) {
+        return ExhibitionType.valueOf(value.toUpperCase());
+    }
+
+    @JsonValue
+    public String toJson() {
+        return name().toLowerCase();
+    }
 }
