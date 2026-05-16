3ea5ee2b692383e1c9d926c64dc2143c5e8fc91c
Yeonhee-Ha <flfkrdl123@gmail.com>
2026-05-16 21:25:29 +0900

    Refactor: 지도 이미지 단건 업로드로 변환

--- src/main/java/com/dolog/server/domain/exhibition/service/ExhibitionGuideMapService.java
+++ src/main/java/com/dolog/server/domain/exhibition/service/ExhibitionGuideMapService.java
@@ -1,9 +1,11 @@
 package com.dolog.server.domain.exhibition.service;
 
 import com.dolog.server.domain.exhibition.web.dto.request.guideMap.ExhibitionGuideMapCreateRequest;
-import java.util.List;
 import java.util.UUID;
 
 public interface ExhibitionGuideMapService {
-    void createGuideMaps(UUID exhibitionId, List<ExhibitionGuideMapCreateRequest.GuideMapRequest> requests);
+    void createGuideMap(
+            UUID exhibitionId,
+            ExhibitionGuideMapCreateRequest request
+    );
 }
