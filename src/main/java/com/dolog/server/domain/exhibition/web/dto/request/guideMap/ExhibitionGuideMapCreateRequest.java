1a4d4b0dead8bf1a7b1880dfb82ee0c5a0d7602c
Yeonhee-Ha <flfkrdl123@gmail.com>
2026-05-16 22:25:46 +0900

    Bug: Setter 추가

--- src/main/java/com/dolog/server/domain/exhibition/web/dto/request/guideMap/ExhibitionGuideMapCreateRequest.java
+++ src/main/java/com/dolog/server/domain/exhibition/web/dto/request/guideMap/ExhibitionGuideMapCreateRequest.java
@@ -1,12 +1,16 @@
 package com.dolog.server.domain.exhibition.web.dto.request.guideMap;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.UUID;
 
+@Data
 @Getter
+@Setter
 @NoArgsConstructor
 public class ExhibitionGuideMapCreateRequest {
 
